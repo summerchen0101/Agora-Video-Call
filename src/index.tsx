e1185979d2288c74f8a337f035da4ccdbd2ec08b
Summer Chen <mp4bj6ruby@gmail.com>
2020-10-29 10:15:17 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import store from '@/store';
+// import App from '@/components/App';
+
+ReactDOM.render(
+  <>
+    <h1>Hello World!</h1>
+  </>,
+  document.getElementById('root'),
+);
